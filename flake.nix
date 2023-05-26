66d7701842850e6b460aca8a63aa00f89f521663
Jasper Chan <jasperchan515@gmail.com>
2023-05-26 00:54:56 -0700

    enable agenix

--- flake.nix
+++ flake.nix
@@ -7,9 +7,13 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ { nixpkgs, home-manager, ... }:
+  outputs = inputs @ { nixpkgs, home-manager, agenix, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -25,6 +29,7 @@
         inherit system;
         modules = [
           ./configuration.nix
+          agenix.nixosModules.default
           ./hosts/virtualbox/configuration.nix
           ./hosts/virtualbox/hw-config.nix
           ./users/jasper/user.nix

3ba5795a0dfc64f32cd8af5788a21b8122e1b178
Jasper Chan <jasperchan515@gmail.com>
2023-11-06 18:15:56 -0800

    secrets: add jasper-pc-wsl pubkey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,14 @@
 let
   jasper-virtualbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILBoOvyE+sGI2jS1owqCXlHpqZcVOSrJwe6QPH5pnTpq";
-  users = [ jasper-virtualbox ];
+  jasper-pc-wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjA/h30yvAJQkkfMBncAKo2aY1dzb+2m/eWw3MLfV76";
+  users = [ jasper-pc-wsl jasper-virtualbox ];
 
   virtualbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFeQvEjbv20hcOaJ4RpzrC5eojf1FGG8fN4h9g8vHU/q";
   systems = [ virtualbox ];
 in {
-  "alert-outlook.age".publicKeys = [ jasper-virtualbox virtualbox ];
-  "jasper-virtualbox.age".publicKeys = [ jasper-virtualbox virtualbox ];
-  "samba-virtualbox.age".publicKeys = [ jasper-virtualbox virtualbox ];
+  "alert-outlook.age".publicKeys = users ++ systems;
+  "jasper-virtualbox.age".publicKeys = users ++ systems;
+  "samba-virtualbox.age".publicKeys = users ++ systems;
 }
 
 

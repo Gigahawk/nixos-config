3caccf6d5a9fcbb336084acd72f292399a3e71a5
Jasper Chan <jasperchan515@gmail.com>
2023-11-15 22:59:24 -0800

    ptolemy: update password

--- users/jasper/user.nix
+++ users/jasper/user.nix
@@ -3,7 +3,7 @@
 {
   users.users.jasper = {
     isNormalUser = true;
-    passwordFile = config.age.secrets.jasper.path;
+    hashedPasswordFile = config.age.secrets.jasper.path;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjA/h30yvAJQkkfMBncAKo2aY1dzb+2m/eWw3MLfV76 jasper"

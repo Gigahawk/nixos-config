6e25d780cdfe43ee3b8a66733fb192a1c3c33f5f
Jasper Chan <jasperchan515@gmail.com>
2023-11-16 00:59:55 -0800

    ptolemy: copy missing microcode setting from generated hw config

--- hosts/ptolemy/hw-config.nix
+++ hosts/ptolemy/hw-config.nix
@@ -88,5 +88,6 @@
   # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
 

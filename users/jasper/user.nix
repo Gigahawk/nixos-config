90982beb871acb0b45e2cc3fcbbbb78717cb7f63
Jasper Chan <jasperchan515@gmail.com>
2024-06-12 06:36:12 +0000

    users: add veda ssh pubkey to jasper

--- users/jasper/user.nix
+++ users/jasper/user.nix
@@ -6,7 +6,10 @@
     hashedPasswordFile = config.age.secrets.jasper.path;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
+      # JASPER-PC Ubuntu WSL
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjA/h30yvAJQkkfMBncAKo2aY1dzb+2m/eWw3MLfV76 jasper"
+      # veda
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuW7/do+fI6PCEQdb+Ui0zBPlZvo/YKf5Nl6uujoPl2 jasper"
     ];
   };
 }

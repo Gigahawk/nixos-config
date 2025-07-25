2063660a2ecb3746bb2edfe54ef857e6d8d845b3
Jasper Chan <jasperchan515@gmail.com>
2025-07-24 22:53:04 -0700

    jasper: add virtualbox key

--- users/jasper/user.nix
+++ users/jasper/user.nix
@@ -13,6 +13,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjA/h30yvAJQkkfMBncAKo2aY1dzb+2m/eWw3MLfV76 jasper"
       # veda
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuW7/do+fI6PCEQdb+Ui0zBPlZvo/YKf5Nl6uujoPl2 jasper"
+      # virtualbox
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILBoOvyE+sGI2jS1owqCXlHpqZcVOSrJwe6QPH5pnTpq jasper"
     ];
   };
 }

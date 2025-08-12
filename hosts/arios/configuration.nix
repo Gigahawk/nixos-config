4955e5d249cb03b183fd714c8fa5f7f795f80eb5
Jasper Chan <jasperchan515@gmail.com>
2025-08-12 00:43:05 -0700

    chore: format arios config

--- hosts/arios/configuration.nix
+++ hosts/arios/configuration.nix
@@ -1,22 +1,24 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
- 
-{ config, lib, pkgs, ... }:
- 
 {
-  imports = [ ];
- 
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [];
+
   networking.hostName = "arios";
   # Seems like using networking.wireless doesn't work super well on
   # interactive systems, use networkmanager for now
   networking.networkmanager = {
     enable = true;
   };
- 
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
- 
+
   services.displayManager.ly = {
     enable = true;
     settings = {
@@ -30,7 +32,7 @@
     enable = true;
     xwayland.enable = true;
   };
- 
+
   services.openssh.enable = true;
 
   environment.systemPackages = with pkgs; [
@@ -56,7 +58,7 @@
       file = ../../secrets/wifi-env.age;
     };
   };
- 
+
   system.stateVersion = "25.05";
- 
 }
+

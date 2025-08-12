dc5817f8d5094ef3bdc7df36a399f409f068ad75
Jasper Chan <jasperchan515@gmail.com>
2025-08-11 23:49:24 -0700

    chore: format home manager file

--- users/jasper/home.nix
+++ users/jasper/home.nix
@@ -1,12 +1,12 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   home.username = "jasper";
   home.homeDirectory = "/home/jasper";
 
   home.stateVersion = "23.05";
 
   programs.home-manager.enable = true;
-  
 }
-

6b325d07204d0b9a04baef7fa982eeb12142a3d2
Jasper Chan <jasperchan515@gmail.com>
2025-09-12 01:52:15 -0700

    arios: add kernel patch to fix tailscale compile

--- hosts/arios/hw-config.nix
+++ hosts/arios/hw-config.nix
@@ -17,6 +17,20 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
+  # https://github.com/tailscale/tailscale/issues/16966#issuecomment-3246030218
+  boot.kernelPatches = [
+    # Fix the /proc/net/tcp seek issue
+    # Impacts tailscale: https://github.com/tailscale/tailscale/issues/16966
+    {
+      name = "proc: fix missing pde_set_flags() for net proc files";
+      patch = pkgs.fetchurl {
+        name = "fix-missing-pde_set_flags-for-net-proc-files.patch";
+        url = "https://patchwork.kernel.org/project/linux-fsdevel/patch/20250821105806.1453833-1-wangzijie1@honor.com/raw/";
+        hash = "sha256-DbQ8FiRj65B28zP0xxg6LvW5ocEH8AHOqaRbYZOTDXg=";
+      };
+    }
+  ];
+
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;

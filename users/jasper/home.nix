b7d7bfd32f8168eb3c1d36f56a3e990e2ea290c0
Jasper Chan <jasperchan515@gmail.com>
2025-10-10 03:18:19 -0700

    general: enable hyprpanel
    * disable fnott for now for hyprpanel

--- users/jasper/home.nix
+++ users/jasper/home.nix
@@ -121,9 +121,9 @@ in
     };
   };
 
-  services.fnott = {
-    enable = desktop;
-  };
+  #services.fnott = {
+  #  enable = desktop;
+  #};
 
   services.udiskie = {
     enable = true;
@@ -611,6 +611,57 @@ in
           background-color: #0069d4;
       }
     '';
+  };
+
+  programs.hyprpanel = {
+    enable = true;
+    dontAssertNotificationDaemons = true;
+    # Configure and theme almost all options from the GUI.
+    # See 'https://hyprpanel.com/configuration/settings.html'.
+    # Default: <same as gui>
+    settings = {
+
+      # Configure bar layouts for monitors.
+      # See 'https://hyprpanel.com/configuration/panel.html'.
+      # Default: null
+      layout = {
+        bar.layouts = {
+          "0" = {
+            left = [
+              "dashboard"
+              "workspaces"
+            ];
+            middle = [ "media" ];
+            right = [
+              "volume"
+              "systray"
+              "notifications"
+            ];
+          };
+        };
+      };
+
+      bar.launcher.autoDetectIcon = true;
+      bar.workspaces.show_icons = true;
+
+      menus.clock = {
+        time = {
+          military = true;
+          hideSeconds = true;
+        };
+        weather.unit = "metric";
+      };
+
+      menus.dashboard.directories.enabled = false;
+      menus.dashboard.stats.enable_gpu = true;
+
+      theme.bar.transparent = true;
+
+      theme.font = {
+        #name = "CaskaydiaCove NF";
+        size = "16px";
+      };
+    };
   };
 
   programs.ghostty = {

9219b68db281f53b98579d188dcd3c699454eb1e
Jasper Chan <jasperchan515@gmail.com>
2025-10-27 22:01:35 -0700

    bugfix: fix aiohttp hash

--- overlays/python.nix
+++ overlays/python.nix
@@ -2,10 +2,9 @@ final: prev: rec {
   python3 = prev.python3.override {
     packageOverrides = self: super: {
       aiohttp = super.aiohttp.overrideAttrs (old: {
-        # idk this should break
         src = final.fetchPypi {
           inherit (old) pname version;
-          sha256 = "sha256-+Y8mX7k3Uu+5y2mX1k3K3Y5h3K3Y5h3K3Y5h3K3Y5h3K="; # Example hash, replace with actual
+          sha256 = "sha256-T8YThenJjXL830fm3YGDP0ey93wRTCnNZKNhvlenY6I=";
         };
         # Tests keep failing, seems to be more common on slower systems
         doCheck = false;

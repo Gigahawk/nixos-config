0b0ce4c899214ced53944abc7a3aba067c20acbf
Jasper Chan <jasperchan515@gmail.com>
2025-11-25 12:03:21 -0800

    chore: update aiohttp hash

--- overlays/python.nix
+++ overlays/python.nix
@@ -4,7 +4,7 @@ final: prev: rec {
       aiohttp = super.aiohttp.overrideAttrs (old: {
         src = final.fetchPypi {
           inherit (old) pname version;
-          sha256 = "sha256-T8YThenJjXL830fm3YGDP0ey93wRTCnNZKNhvlenY6I=";
+          sha256 = "sha256-QBdqUsGGrv726zytLN0wzQbjr76I/oqyr5wLkPIo2so=";
         };
         # Tests keep failing, seems to be more common on slower systems
         doCheck = false;

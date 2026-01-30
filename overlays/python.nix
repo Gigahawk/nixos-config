2de82fa91dfbd82a193b522664992eee02cea1a5
Jasper Chan <jasperchan515@gmail.com>
2026-01-30 15:40:42 -0800

    chore: undo aiohttp overlay

--- overlays/python.nix
+++ overlays/python.nix
@@ -1,14 +1,14 @@
 final: prev: rec {
   python3 = prev.python3.override {
     packageOverrides = self: super: {
-      aiohttp = super.aiohttp.overrideAttrs (old: {
-        src = final.fetchPypi {
-          inherit (old) pname version;
-          sha256 = "sha256-QBdqUsGGrv726zytLN0wzQbjr76I/oqyr5wLkPIo2so=";
-        };
-        # Tests keep failing, seems to be more common on slower systems
-        doCheck = false;
-      });
+      #aiohttp = super.aiohttp.overrideAttrs (old: {
+      #  src = final.fetchPypi {
+      #    inherit (old) pname version;
+      #    sha256 = "sha256-QBdqUsGGrv726zytLN0wzQbjr76I/oqyr5wLkPIo2so=";
+      #  };
+      #  # Tests keep failing, seems to be more common on slower systems
+      #  doCheck = false;
+      #});
     };
   };
 }

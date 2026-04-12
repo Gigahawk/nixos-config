fe676cd3240a4c67342af23ae7f4199dcabbe8c4
Jasper Chan <jasperchan515@gmail.com>
2026-04-12 14:01:15 -0700

    inventree: test update to 1.2.7

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     inventree = {
-      url = "github:Gigahawk/nixos-inventree";
+      url = "github:Gigahawk/nixos-inventree/update-1.2.7";
       #inputs.nixpkgs.follows = "nixpkgs";
     };
     kvmd = {

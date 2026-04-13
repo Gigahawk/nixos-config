d44e27c3443cf3888d2860f897204584bc6c83d3
Jasper Chan <jasperchan515@gmail.com>
2026-04-12 20:21:57 -0700

    inventree: test dev-server pr

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     inventree = {
-      url = "github:Gigahawk/nixos-inventree/update-kicad-2.0.4";
+      url = "github:Gigahawk/nixos-inventree/dev-server";
       #inputs.nixpkgs.follows = "nixpkgs";
     };
     kvmd = {

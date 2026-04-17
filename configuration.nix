94bf75e4a9e913534178a308efb6d55bcab5ce0d
Jasper Chan <jasperchan515@gmail.com>
2026-04-16 19:39:19 -0700

    general: use p10k as zsh theme

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,8 @@
     graphics.enable32Bit = true;
   };
 
+  environment.etc."powerlevel10k/p10k.zsh".source = ./p10k.zsh;
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -82,8 +84,26 @@
         "npm"
         "command-not-found"
       ];
-      theme = "fox";
     };
+
+    promptInit = ''
+      # this act as your ~/.zshrc but for all users (/etc/zshrc)
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      source /etc/powerlevel10k/p10k.zsh
+
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      # double single quotes to escape the dollar char
+      if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+
+      # uncomment if you want to customize your LS_COLORS
+      # https://manpages.ubuntu.com/manpages/plucky/en/man5/dir_colors.5.html
+      #LS_COLORS='...'
+      #export LS_COLORS
+    '';
   };
 
   networking.firewall = {
@@ -122,7 +142,12 @@
     enable = true;
   };
 
-  users.mutableUsers = false;
+  users = {
+    mutableUsers = false;
+    users.root = {
+      shell = pkgs.zsh;
+    };
+  };
 
   security.polkit.enable = true;
 
